a0f8244bc923e3fbf9ac1e3f339796c0aa69117c
Trav3019 <Travismdk30@hotmail.com>
2026-04-17 17:52:04 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import LeaguesPage from './pages/LeaguesPage';
@@ -6,21 +7,33 @@ import LeaguePage from './pages/LeaguePage';
 import PlayerPage from './pages/PlayerPage';
 import AddRoundPage from './pages/AddRoundPage';
 import LeaderboardPage from './pages/LeaderboardPage';
+import FeedPage from './pages/FeedPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import AdminPage from './pages/AdminPage';
+import EditProfilePage from './pages/EditProfilePage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/leagues" element={<LeaguesPage />} />
-          <Route path="/leagues/:leagueId" element={<LeaguePage />} />
-          <Route path="/player/:playerId" element={<PlayerPage />} />
-          <Route path="/player/:playerId/add-round" element={<AddRoundPage />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/feed" element={<FeedPage />} />
+            <Route path="/leagues" element={<LeaguesPage />} />
+            <Route path="/leagues/:leagueId" element={<LeaguePage />} />
+            <Route path="/player/:playerId" element={<PlayerPage />} />
+            <Route path="/player/:playerId/add-round" element={<AddRoundPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/edit-profile" element={<EditProfilePage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
